6902b9a959a7aa9a3b55ce1c592551e80c0a9950
allenwind <allenwind@foxmail.com>
2020-11-02 22:40:37 +0800

    auto push

--- namespace.cpp
+++ namespace.cpp
@@ -3,9 +3,15 @@
 using std::cout;
 using std::endl;
 
+namespace A::B::C
+{
+	int a;
+}
+
 int main(int argc, char ** argv)
 {
     
-    
+    A::B::C::a = 1;
+    cout << A::B::C::a << endl;
     return 0;
 }
